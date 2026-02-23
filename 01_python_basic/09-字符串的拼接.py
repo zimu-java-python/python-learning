5ea76fdabdb7ad7487b49153f67c2fe95c9df52b
zimu-java-python <2869758975@qq.com>
2026-02-23 21:04:27 +0800

    Create 09-字符串的拼接.py

---
+++ 01_python_basic/09-字符串的拼接.py
@@ -0,0 +1,13 @@
+# 字符串字面量之间的拼接
+print("学IT来黑马" + "月薪过万")
+
+# 字符串字面量和字符串变量的拼接
+name = "黑马程序员"
+print("我的名字是：" + name + "，我可以教大家IT技能")
+
+name = "黑马程序员"
+address = "建材城东路9号院"
+tel = 15156139172
+tel = str(tel)
+print("我是：" + name + "，我的地址是" + address + "我的电话是：" + tel)
+print(type(tel))
