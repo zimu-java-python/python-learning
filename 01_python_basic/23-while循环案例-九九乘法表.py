9afe24d42eb5f9ca379beeab15c44705aafebb90
zimu-java-python <2869758975@qq.com>
2026-02-23 21:17:52 +0800

    Create 23-while循环案例-九九乘法表.py

---
+++ 01_python_basic/23-while循环案例-九九乘法表.py
@@ -0,0 +1,24 @@
+# print输出不换行
+# print("Hello", end='')
+# print("world",end='')
+
+# \t制表符
+# print("Hello\tworld")
+# print("itheima\tbest")
+
+"""
+练习用while循环打印九九乘法表
+"""
+# 定义外层循环的控制变量
+i = 1
+while i <= 9:
+
+    # 定义内层循环的控制变量
+    j = 1
+    while j <= i:
+        # 内层print语句，不要换行,通过\t制表符进行对齐
+        print(f"{j}*{i}={j*i}\t", end='')
+        j += 1
+
+    i += 1
+    print() # print空内容，就是输出一个换行
