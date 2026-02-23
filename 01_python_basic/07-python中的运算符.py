1d45a627ef0aa874c80822385055f3240468a586
zimu-java-python <2869758975@qq.com>
2026-02-23 21:03:42 +0800

    Create 07-python中的运算符.py

---
+++ 01_python_basic/07-python中的运算符.py
@@ -0,0 +1,31 @@
+# 算数（数学）运算符
+print("1 + 1 =", 1 + 1)
+print("2 - 1 =", 2 - 1)
+print("3 * 3 =", 3 * 3)
+print("4 / 2 =", 4 / 2)
+print("11 // 2 =", 11 // 2)
+print("9 % 2 =", 9 % 2)
+print("2 ** 2 =", 2 ** 2)
+
+# 赋值运算符
+num = 1 +2 * 3
+print("num =", num)
+
+# 复合赋值运算符
+num_1 = 1
+num_1 += 1 # num_1 = num_1 + 1(等效)
+print("num_1 += 1:", num_1)
+num_1 -= 1
+print("num_1 -= 1:", num_1)
+num_1 *= 4
+print("num_1 *= 4:", num_1)
+num_1 /= 2
+print("num_1 /= 2:", num_1)
+num_2 =3
+num_2 %= 2
+print("num_2 %= 2:", num_2)
+num_2 **= 2
+print("num_2 **= 2:", num_2)
+num_3 = 9
+num_3 //= 2
+print("num_3 //= 2:", num_3)
